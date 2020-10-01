59629ff19127763a6ac128c2f555e80d732a081d
Nikolay Kostyurin <jilizart@gmail.com>
2020-10-01 23:13:11 +0200

    chore: fix settings clickable

--- packages/toolbar/src/components/SettingsBar.vue
+++ packages/toolbar/src/components/SettingsBar.vue
@@ -18,19 +18,24 @@ export default {
 
 <template>
   <div class="settings-bar">
-    <Block @click="$emit('fullClick')" v-show="closeVisible">
+    <Block @click="$emit('fullClick')" v-show="closeVisible" :clickable="true">
       <Icon name="fullscreen" :size="size"></Icon>
     </Block>
-    <Block @click="$emit('fullExitClick')" v-show="fullExitVisible">
+    <Block
+      @click="$emit('fullExitClick')"
+      v-show="fullExitVisible"
+      :clickable="true"
+    >
       <Icon name="fullscreen_exit" :size="size"></Icon>
     </Block>
     <Block
       @click="$emit('closeClick')"
       v-show="closeVisible || fullExitVisible"
+      :clickable="true"
     >
       <Icon name="clear" :size="size"></Icon>
     </Block>
-    <Block @click="$emit('settingsClick')">
+    <Block @click="$emit('settingsClick')" :clickable="true">
       <Icon name="more_vert" :size="size"></Icon>
     </Block>
   </div>
